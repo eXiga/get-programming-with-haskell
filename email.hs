0ce669a0a3cb82941d45b0d69e7cac8488d35a26
eXiga <anton.kostenich@hotmail.com>
2018-11-21 01:37:49 +0100

    Initial commit with create email program

---
+++ email.hs
@@ -0,0 +1,22 @@
+main :: IO ()
+main = do
+  print "Who is the email for?"
+  recipient <- getLine
+  print "What is the Title?"
+  title <- getLine
+  print "Who is the Author?"
+  author <- getLine
+  print (createEmail recipient title author)
+
+toPart :: String -> String
+toPart recipient = "Dear " ++ recipient ++ ",\n"
+
+bodyPart :: String -> String
+bodyPart bookTitle = "Thanks for buying " ++ bookTitle ++ ".\n"
+
+fromPart :: String -> String
+fromPart author = "Thanks,\n" ++ author
+
+createEmail :: String -> String -> String -> String
+createEmail recipient bookTitle author =
+  toPart recipient ++ bodyPart bookTitle ++ fromPart author
