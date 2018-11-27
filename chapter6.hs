ebac798b024f958fae7ef9a2da60b9f9d2f594d5
eXiga <anton.kostenich@hotmail.com>
2018-11-27 19:23:53 +0100

    Complete exercises from ch. 6

---
+++ chapter6.hs
@@ -0,0 +1,13 @@
+module Lib where
+
+cRepeat :: a -> [a]
+cRepeat n = cycle [n]
+
+subseq :: Int -> Int -> [a] -> [a]
+subseq startIndex endIndex aList =
+  take (endIndex - startIndex) (drop startIndex aList)
+
+inFirstHalf :: (Eq a) => a -> [a] -> Bool
+inFirstHalf n aList = n `elem` take half aList
+  where
+    half = length aList `div` 2
