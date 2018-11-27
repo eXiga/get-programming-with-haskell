6d37e72050355c8fcbe577d792035f675f7c0710
eXiga <anton.kostenich@hotmail.com>
2018-11-27 21:23:01 +0100

    Complete exercises from ch. 7

---
+++ chapter7.hs
@@ -0,0 +1,21 @@
+module Lib where
+
+cGcd :: Integral a => a -> a -> a
+cGcd a b =
+  if remainder == 0
+    then b
+    else cGcd b remainder
+  where
+    remainder = a `mod` b
+
+cHead :: [a] -> a
+cHead (x:_) = x
+cHead [] = error "No head for empty list"
+
+cTail :: [a] -> [a]
+cTail (_:xs) = xs
+cTail [] = []
+
+improvedGcd :: Integral a => a -> a -> a
+improvedGcd a 0 = a
+improvedGcd a b = improvedGcd b (a `mod` b)
