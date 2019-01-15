dc92375f0793709a94229b3a2f5c4d0f70677c2f
eXiga <anton.kostenich@hotmail.com>
2019-01-15 02:01:39 +0100

    Add out of bounds guard to q.2 from ch. 22, fix function names

--- chapter22.hs
+++ chapter22.hs
@@ -1,20 +1,24 @@
---module Chapter22 where
-import Data.Char (digitToInt, isSpace)
-import System.Environment
+module Chapter22 where
 
+import Data.Char (digitToInt, isSpace)
+
+-- Quick check 1
 qc1 :: IO ()
 qc1 = do
   vals <- mapM (const getLine) [1 .. 3]
   mapM_ putStrLn vals
 
+-- Quick check 2
 qc2 :: Monad m => Int -> m a -> m [a]
 qc2 n m = mapM (const m) [1 .. n]
 
+-- Quick check 3
 qc3 :: IO ()
 qc3 = do
   userInput <- getContents
   putStrLn $ reverse userInput
 
+-- Quick check 4
 toInts :: String -> [Int]
 toInts = map read . lines
 
@@ -23,6 +27,7 @@ qc4 = do
   userInput <- getContents
   print $ sum $ map (^ 2) $ toInts userInput
 
+-- Final question 1
 parseOperator :: (Num a) => Char -> a -> a -> a
 parseOperator '+' = (+)
 parseOperator '*' = (*)
@@ -38,6 +43,7 @@ task1 = do
   userInput <- getContents
   print $ map calculate $ lines userInput
 
+-- Final question 2
 quotes :: [String]
 quotes = ["Quote 1", "Quote 2", "Quote 3", "Quote 4", "Quote 5"]
 
@@ -46,9 +52,11 @@ findQuote [] = []
 findQuote ("n":_) = []
 findQuote (x:xs) = quote : findQuote xs
   where
-    quote = quotes !! (read x - 1)
+    quote
+      | read x `elem` [1 .. length quotes] = quotes !! (read x - 1)
+      | otherwise = "Incorrect number of quote"
 
-main :: IO ()
-main = do
+task2 :: IO ()
+task2 = do
   userInput <- getContents
   mapM_ putStrLn $ findQuote $ lines userInput
